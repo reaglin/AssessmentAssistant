9f3309d03a472f98ab37b470c680b27da8ac62ba
Ron Eaglin <ron.eaglin@gmail.com>
2022-03-10 11:08:22 -0500

    Enumerations Added

---
+++ Pages/Enumerations/Create.cshtml.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using AssessmentAssistant.Data;
+using AssessmentAssistant.Models;
+
+namespace AssessmentAssistant.Pages.Enumerations
+{
+    public class CreateModel : PageModel
+    {
+
+        public readonly AssessmentAssistant.Data.ApplicationDbContext _context;
+        public CreateModel(AssessmentAssistant.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public AssessmentAssistant.Models.Enumerations Enumeration { get; set; }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            // EnteredByUserId is one of the fields entered automatically. This puts 
+            // in the id of the current logged in user. 
+            //string id = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier).Value;
+            //DonationMaterials.EnteredByUserId = id;
+
+            _context.Enumerations.Add(Enumeration);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
