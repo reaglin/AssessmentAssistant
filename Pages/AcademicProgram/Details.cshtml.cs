c33fc6b01f7bc4a57d1430947e730ae8c4246945
Ron Eaglin <ron.eaglin@gmail.com>
2022-03-22 15:14:51 -0400

    Adds Program Outcomes to Academic Program

--- Pages/AcademicProgram/Details.cshtml.cs
+++ Pages/AcademicProgram/Details.cshtml.cs
@@ -22,6 +22,8 @@ namespace AssessmentAssistant.Pages.AcademicProgram
 
         public AssessmentAssistant.Models.AcademicProgram AcademicProgram { get; set; }
         public List<AssessmentAssistant.Models.AcademicCourse> AcademicCourses { get; set;}
+        public List<AssessmentAssistant.Models.ProgramOutcome> ProgramOutcomes { get; set;}
+
         public async Task<IActionResult> OnGetAsync(int? id)
         {
             if (id == null)
@@ -29,21 +31,40 @@ namespace AssessmentAssistant.Pages.AcademicProgram
                 return NotFound();
             }
 
-            // Explicit Loading
-            // https://docs.microsoft.com/en-us/aspnet/core/data/ef-rp/read-related-data?view=aspnetcore-6.0&tabs=visual-studio
-            AcademicCourses = new List<AssessmentAssistant.Models.AcademicCourse>();
+            // Load the Academic Program - return a Not Found if program is not found
             AcademicProgram = await _context.AcademicPrograms.FirstOrDefaultAsync(m => m.AcademicProgramId == id);
-
-            // Load the courses
-            _context.AcademicCourses.Where(c => c.AcademicProgramId == AcademicProgram.AcademicProgramId).Load();
-            foreach(Models.AcademicCourse c in AcademicProgram.AcademicCourses)
-            {
-                AcademicCourses.Add(c);
-            }
-
             if (AcademicProgram == null)
             {
                 return NotFound();
+            }
+
+
+            // Explicit Loading of Associated Courses
+            // https://docs.microsoft.com/en-us/aspnet/core/data/ef-rp/read-related-data?view=aspnetcore-6.0&tabs=visual-studio
+            AcademicCourses = new List<AssessmentAssistant.Models.AcademicCourse>();
+            
+            // Load the courses
+            _context.AcademicCourses.Where(c => c.AcademicProgramId == AcademicProgram.AcademicProgramId).Load();
+            if (AcademicProgram.AcademicCourses != null) 
+            { 
+                foreach(Models.AcademicCourse c in AcademicProgram.AcademicCourses)
+                {
+                    AcademicCourses.Add(c);
+                }
+            }
+
+            // Explicit Loading of Program Outcomes
+            // https://docs.microsoft.com/en-us/aspnet/core/data/ef-rp/read-related-data?view=aspnetcore-6.0&tabs=visual-studio
+            ProgramOutcomes = new List<AssessmentAssistant.Models.ProgramOutcome>();
+
+            // Load the outcomes
+            _context.ProgramOutcomes.Where(po => po.AcademicProgramId == AcademicProgram.AcademicProgramId).Load();
+
+            if (AcademicProgram.ProgramOutcomes != null) { 
+                foreach (Models.ProgramOutcome po in AcademicProgram.ProgramOutcomes)
+                {
+                    ProgramOutcomes.Add(po);
+                }
             }
             return Page();
         }
